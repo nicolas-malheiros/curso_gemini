84c7b5e2a5fff64b69a6e138fddcb55c3c6a3ad3
nicolasdanielmalheiros@gmail.com <nicolasm@uniplastperfis.com.br>
2025-05-22 08:54:33 -0300

    analisar sentimentos das avaliacoes

---
+++ analisador_sentimentos.py
@@ -0,0 +1,54 @@
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CHAVE_API_GOOGLE = os.getenv("GEMINI_API_KEY")
+genai.configure(api_key=CHAVE_API_GOOGLE)
+MODELO = "gemini-1.5-flash"
+
+def carrega(nome_do_arquivo):
+    try:
+        with open(nome_do_arquivo, "r") as arquivo:
+            dados = arquivo.read()
+            return dados
+    except IOError as e:
+        print(f"Erro: {e}")
+
+def salva(nome_do_arquivo, conteudo):
+    try:
+        with open(nome_do_arquivo, "w", encoding="utf-8") as arquivo:
+            arquivo.write(conteudo)
+    except IOError as e:
+        print(f"Erro ao salvar arquivo: {e}")
+
+prompt_sistema = f"""
+        Você é um analisador de sentimentos de avaliações de produtos.
+        Escreva um parágrafo com até 50 palavras resumindo as avaliações e
+        depois atribua qual o sentimento geral para o produto.
+        Identifique também 3 pontos fortes e 3 pontos fracos identificados a partir das avaliações.
+
+        # Formato de Saída
+
+        Nome do Produto:
+        Resumo das Avaliações:
+        Sentimento Geral: [utilize aqui apenas Positivo, Negativo ou Neutro]
+        Ponto fortes: lista com três bullets
+        Pontos fracos: lista com três bullets
+    """
+
+nome_produto = "Camisetas de algodão orgânico"
+prompt_usuario = carrega(f"dados/avaliacoes-{nome_produto}.txt")
+
+print(f"Iniciando a análise de sentimentos do produto: {nome_produto}")
+
+llm = genai.GenerativeModel(
+    model_name=MODELO,
+    system_instruction=prompt_sistema
+)
+
+resposta = llm.generate_content(prompt_usuario)
+texto_resposta = resposta.text
+
+salva(f"dados/resposta-{nome_produto}", texto_resposta)
