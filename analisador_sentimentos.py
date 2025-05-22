f810f5b2134435a88a383a09ea90312f43a0d72f
nicolasdanielmalheiros@gmail.com <nicolasm@uniplastperfis.com.br>
2025-05-22 09:15:32 -0300

    tratamento de erro

--- analisador_sentimentos.py
+++ analisador_sentimentos.py
@@ -1,12 +1,13 @@
 import os
 import google.generativeai as genai
+from google.api_core.exceptions import NotFound
 from dotenv import load_dotenv
 
 load_dotenv()
 
 CHAVE_API_GOOGLE = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=CHAVE_API_GOOGLE)
-MODELO = "gemini-1.5-flash"
+modelo = "gemini-1.5-flash"
 
 def carrega(nome_do_arquivo):
     try:
@@ -23,32 +24,46 @@ def salva(nome_do_arquivo, conteudo):
     except IOError as e:
         print(f"Erro ao salvar arquivo: {e}")
 
-prompt_sistema = f"""
-        Você é um analisador de sentimentos de avaliações de produtos.
-        Escreva um parágrafo com até 50 palavras resumindo as avaliações e
-        depois atribua qual o sentimento geral para o produto.
-        Identifique também 3 pontos fortes e 3 pontos fracos identificados a partir das avaliações.
+def analisador_sentimentos(nome_produto, modelo="gemini-1.5-flash"):
+    prompt_sistema = f"""
+            Você é um analisador de sentimentos de avaliações de produtos.
+            Escreva um parágrafo com até 50 palavras resumindo as avaliações e
+            depois atribua qual o sentimento geral para o produto.
+            Identifique também 3 pontos fortes e 3 pontos fracos identificados a partir das avaliações.
 
-        # Formato de Saída
+            # Formato de Saída
 
-        Nome do Produto:
-        Resumo das Avaliações:
-        Sentimento Geral: [utilize aqui apenas Positivo, Negativo ou Neutro]
-        Ponto fortes: lista com três bullets
-        Pontos fracos: lista com três bullets
-    """
+            Nome do Produto:
+            Resumo das Avaliações:
+            Sentimento Geral: [utilize aqui apenas Positivo, Negativo ou Neutro]
+            Ponto fortes: lista com três bullets
+            Pontos fracos: lista com três bullets
+        """
 
-nome_produto = "Camisetas de algodão orgânico"
-prompt_usuario = carrega(f"dados/avaliacoes-{nome_produto}.txt")
+    prompt_usuario = carrega(f"dados/avaliacoes-{nome_produto}.txt")
 
-print(f"Iniciando a análise de sentimentos do produto: {nome_produto}")
+    print(f"Iniciando a análise de sentimentos do produto: {nome_produto}")
 
-llm = genai.GenerativeModel(
-    model_name=MODELO,
-    system_instruction=prompt_sistema
-)
+    try:
+        llm = genai.GenerativeModel(
+            model_name=modelo,
+            system_instruction=prompt_sistema
+        )
 
-resposta = llm.generate_content(prompt_usuario)
-texto_resposta = resposta.text
+        resposta = llm.generate_content(prompt_usuario)
+        texto_resposta = resposta.text
 
-salva(f"dados/resposta-{nome_produto}", texto_resposta)
+        salva(f"dados/resposta-{nome_produto}", texto_resposta)
+    except NotFound as e:
+        modelo = "gemini-1.5-flash"
+        print(f"Erro no nome do modelo: {e}")
+        analisador_sentimentos(nome_produto, modelo)
+
+def main():
+    lista_de_produtos = ["Camisetas de algodão orgânico", "Jeans feitos com materiais reciclados", "Maquiagem mineral"]
+
+    for um_produto in lista_de_produtos:
+        analisador_sentimentos(um_produto, modelo)
+
+if __name__ == "__main__":
+    main()
